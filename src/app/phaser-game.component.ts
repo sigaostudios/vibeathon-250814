c1a283b6f3248962702f1a7e13ce34c292a0a8cb
chris <chris@sigao.io>
2025-08-07 15:36:27 -0500

    Fix dev script to use log.js and enforce app- selector prefix (update component + usage)

--- src/app/phaser-game.component.ts
+++ src/app/phaser-game.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from "@angular/core";
-import Phaser from "phaser";
-import StartGame from "../game/main";
-import { EventBus } from "../game/EventBus";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import Phaser from 'phaser';
+import StartGame from '../game/main';
+import { EventBus } from '../game/EventBus';
+import { StorageService } from './storage.service';
 
 @Component({
-    selector: 'phaser-game',
+    selector: 'app-phaser-game',
     template: '<div id="game-container"></div>',
     standalone: true,
 })
-export class PhaserGame implements OnInit
+export class PhaserGameComponent implements OnInit, OnDestroy
 {
     scene: Phaser.Scene;
     game: Phaser.Game;
     sceneCallback: (scene: Phaser.Scene) => void;
+    private loadedConfig: any;
+
+    constructor(private storage: StorageService) {}
 
     ngOnInit ()
     {
         this.game = StartGame('game-container');
+
+        // Load any saved game configuration (if present)
+        this.storage.getItem<any>('gameConfig').then(cfg => {
+            this.loadedConfig = cfg;
+        });
 
         EventBus.on('current-scene-ready', (scene: Phaser.Scene) =>
         {
